205490ce6692e6464ba0c1a850514a7bbde2b8b5
James Frowen <jamesfrowendev@gmail.com>
2022-03-29 14:20:26 +0100

    starting on socket

--- Assets/EpicOnlineTransport/Transport/EpicSocketFactory.cs
+++ Assets/EpicOnlineTransport/Transport/EpicSocketFactory.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Net;
+using Epic.Logging;
+using Epic.OnlineServices.P2P;
 using Mirage.SocketLayer;
 using PlayEveryWare.EpicOnlineServices;
 using UnityEngine;
@@ -10,13 +12,7 @@ namespace EpicServicesPeer
     [DisallowMultipleComponent]
     public class EpicSocketFactory : SocketFactory
     {
-        #region Fields
-
         [SerializeField] private EpicOptions _options = new EpicOptions();
-
-        #endregion
-
-        #region Overrides of SocketFactory
 
         /// <summary>Max size for packets sent to or received from Socket
         /// <para>Called once when Sockets are created</para></summary>
@@ -26,30 +22,87 @@ namespace EpicServicesPeer
         /// <exception cref="NotSupportedException">Throw when Server is not supported on current platform</exception>
         public override ISocket CreateServerSocket()
         {
-            throw new System.NotImplementedException();
+            return new EpicSocket(_options);
         }
 
         /// <summary>Creates the <see cref="EndPoint"/> that the Server Socket will bind to</summary>
         /// <exception cref="NotSupportedException">Throw when Client is not supported on current platform</exception>
         public override IEndPoint GetBindEndPoint()
         {
-            throw new System.NotImplementedException();
+            return new EpicEndPoint();
         }
 
         /// <summary>Creates a <see cref="ISocket"/> to be used by <see cref="Peer"/> on the client</summary>
         /// <exception cref="NotSupportedException">Throw when Client is not supported on current platform</exception>
         public override ISocket CreateClientSocket()
         {
-            throw new System.NotImplementedException();
+            return new EpicSocket(_options);
         }
 
         /// <summary>Creates the <see cref="EndPoint"/> that the Client Socket will connect to using the parameter given</summary>
         /// <exception cref="NotSupportedException">Throw when Client is not supported on current platform</exception>
         public override IEndPoint GetConnectEndPoint(string address = null, ushort? port = null)
         {
-            throw new System.NotImplementedException();
+            return new EpicEndPoint(address);
+        }
+    }
+
+    internal sealed class EpicEndPoint : IEndPoint
+    {
+        public string Address;
+
+        public EpicEndPoint() { }
+        public EpicEndPoint(string address)
+        {
+            Address = address;
         }
 
-        #endregion
+        IEndPoint IEndPoint.CreateCopy()
+        {
+            return new EpicEndPoint(Address);
+        }
+    }
+    internal sealed class EpicSocket : ISocket
+    {
+        private readonly EpicOptions _epicOptions;
+        private readonly P2PInterface _p2p;
+
+        public EpicSocket(EpicOptions options)
+        {
+            DebugLogger.RegularDebugLog("[EpicSocket] - Staring up socket.");
+
+            _epicOptions = options;
+            _p2p = EOSManager.Instance.GetEOSP2PInterface();
+        }
+
+        public void Bind(IEndPoint endPoint)
+        {
+            // _p2p.
+        }
+
+        public void Connect(IEndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Poll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Receive(byte[] buffer, out IEndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(IEndPoint endPoint, byte[] packet, int length)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Assets/EpicOnlineTransport/Transport/EpicSocketManager.cs
+++ Assets/EpicOnlineTransport/Transport/EpicSocketManager.cs
@@ -1,10 +1,9 @@
-using System;
 using Cysharp.Threading.Tasks;
 using Epic.Logging;
 using Epic.OnlineServices;
 using Epic.OnlineServices.P2P;
-using Mirage.SocketLayer;
 using PlayEveryWare.EpicOnlineServices;
+using System;
 using UnityEngine;
 
 namespace EpicServicesPeer
@@ -15,10 +14,10 @@ namespace EpicServicesPeer
 
         public EpicSocketManager(EpicOptions options)
         {
-            _epicOptions = options;
+            this._epicOptions = options;
         }
 
-            /// <summary>
+        /// <summary>
         ///     Check to see if we have received any data from epic users.
         /// </summary>
         /// <param name="clientProductUserId">Returns back the epic id of users who sent message.</param>
@@ -78,19 +77,19 @@ namespace EpicServicesPeer
             {
                 while (true)
                 {
-                    for (int chNum = 0; chNum <= _epicOptions.Channels.Length; chNum++)
+                    for (int chNum = 0; chNum <= this._epicOptions.Channels.Length; chNum++)
                     {
-                        while (DataAvailable(out ProductUserId clientUserID, out byte[] receiveBuffer, (byte)chNum,
+                        while (this.DataAvailable(out ProductUserId clientUserID, out byte[] receiveBuffer, (byte)chNum,
                                    out SocketId socket))
                         {
 
-                            switch (chNum == _epicOptions.Channels.Length)
+                            switch (chNum == this._epicOptions.Channels.Length)
                             {
                                 case true:
-                                    OnReceiveInternalData((InternalMessage)receiveBuffer[0], clientUserID, socket);
+                                    this.OnReceiveInternalData((InternalMessage)receiveBuffer[0], clientUserID, socket);
                                     break;
                                 case false:
-                                    OnReceiveData(receiveBuffer, clientUserID, chNum);
+                                    this.OnReceiveData(receiveBuffer, clientUserID, chNum);
                                     break;
                             }
                         }
@@ -116,90 +115,4 @@ namespace EpicServicesPeer
         #endregion
     }
 
-    internal sealed class EpicSocket : ISocket
-    {
-        #region Fields
-
-        private readonly EpicOptions _epicOptions;
-        private readonly EpicSocketManager _epicSocketManager;
-
-        #endregion
-
-        public EpicSocket(EpicOptions options)
-        {
-            DebugLogger.RegularDebugLog("[EpicSocket] - Staring up socket.");
-
-            _epicOptions = options;
-            _epicSocketManager = new EpicSocketManager(options);
-
-        }
-
-        #region Implementation of ISocket
-
-        /// <summary>
-        /// Starts listens for data on an endpoint
-        /// <para>Used by Server to allow clients to connect</para>
-        /// </summary>
-        /// <param name="endPoint">the endpoint to listen on</param>
-        public void Bind(IEndPoint endPoint)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Sets up Socket ready to send data to endpoint as a client
-        /// </summary>
-        /// <param name="endPoint"></param>
-        public void Connect(IEndPoint endPoint)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Closes the socket, stops receiving messages from other peers
-        /// </summary>
-        public void Close()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Checks if a packet is available 
-        /// </summary>
-        /// <returns>true if there is atleast 1 packet to read</returns>
-        public bool Poll()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Gets next packet
-        /// <para>Should be called after Poll</para>
-        /// <para>
-        ///     Implementation should check that incoming packet is within the size of <paramref name="buffer"/>,
-        ///     and make sure not to return <paramref name="bytesReceived"/> above that size
-        /// </para>
-        /// </summary>
-        /// <param name="buffer">buffer to write recevived packet into</param>
-        /// <param name="endPoint">where packet came from</param>
-        /// <returns>length of packet, should not be above <paramref name="buffer"/> length</returns>
-        public int Receive(byte[] buffer, out IEndPoint endPoint)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Sends a packet to an endpoint
-        /// <para>Implementation should use <paramref name="length"/> because <paramref name="packet"/> is a buffer than may contain data from previous packets</para>
-        /// </summary>
-        /// <param name="endPoint">where packet is being sent to</param>
-        /// <param name="packet">buffer that contains the packet, starting at index 0</param>
-        /// <param name="length">length of the packet</param>
-        public void Send(IEndPoint endPoint, byte[] packet, int length)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-    }
 }
